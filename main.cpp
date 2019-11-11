3c55bfc64ae793632d7957227ec069b628ed5d25
Alexey <volkov.aa@phystech.edu>
2019-11-11 03:28:25 +0300

    Impemented node_t and tree_t structures

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+
+struct node_t {
+    node_t *left;
+    node_t *right;
+    node_t *parent;
+    void *value;
+};
+
+struct tree_t {
+    node_t *head;
+    size_t size;
+};
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
